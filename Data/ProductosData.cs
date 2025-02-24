621efb3cd365f9a5cb954b186a8264de4185dac9
danielcantillo0 <danielcantillo8@gmail.com>
2025-02-24 07:55:07 -0500

    Commiting changes

---
+++ Data/ProductosData.cs
@@ -0,0 +1,102 @@
+﻿namespace CanvelStaticCpanel.Data
+{
+    public class ProductosData
+    {
+        public class ProductDto
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+        }
+
+        public List<ProductDto> products = new List<ProductDto>
+            {
+                new ProductDto
+                {
+                    Name = "Accesorios para Muelles y Puertos",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FAccesorios%20para%20Muelles%20y%20Puertos.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Boyas",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FBoyas.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Bridas",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FBridas.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Cadenas",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FCadenas.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Cimentaciones para Energía Eólica Offshore",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FCimentaciones%20para%20Energ%C3%ADa%20E%C3%B3lica%20Offshore.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Defensas para Embarcaciones",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FDefensas%20para%20Embarcaciones.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Defensas Flotantes",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FDefensas_Flotantes.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Defensas Marinas",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FDefensas_Marinas.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Ganchos Pelicanos",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FGanchos%20Pelicanos.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Mangueras Especiales",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FMangueras%20Especiales.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Mangueras Marinas",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FMangueras%20Marinas.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Productos Offshore",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FProductos%20Offshore.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Sistemas Anticontaminación",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FSistemas%20Anticontaminaci%C3%B3n.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Soluciones de Amarre",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FSoluciones%20de%20Amarre.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Spool Pieces",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FSpool%20Pieces.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Válvulas",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FV%C3%A1lvulas.png?alt=media"
+                },
+                new ProductDto
+                {
+                    Name = "Winker Lights",
+                    Url = "https://firebasestorage.googleapis.com/v0/b/canvel-b3103.appspot.com/o/Assets%2FWinker%20Lights.png?alt=media"
+                }
+            };
+
+        
+    }
+}
