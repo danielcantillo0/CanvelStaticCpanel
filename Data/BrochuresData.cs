d1f472a6e64c4bc208b14ee81999242a0b5e0ec2
danielcantillo0 <danielcantillo8@gmail.com>
2025-02-27 09:49:20 -0500

    wip

---
+++ Data/BrochuresData.cs
@@ -0,0 +1,27 @@
+﻿namespace CanvelStaticCpanel.Data
+{
+    public class BrochuresData
+    {
+        public class BrochureDto
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+        }
+
+        public List<BrochureDto> brochures = new List<BrochureDto>()
+        {
+          new BrochureDto { Name = "IRM Hydro Pneumatic Fenders", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fqubre_8.Hydro%20Pneumatic%20Fenders.pdf?alt=media&token=fd560e68-c189-4981-ab56-d5688050f7e3" },
+          new BrochureDto { Name = "IRM Defensas Flotantes", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fjngl5_3.Floating%20Fenders.pdf?alt=media&token=6c701fa3-0dd7-4021-8408-834e6efc7764" },
+          new BrochureDto { Name = "IRM Defensas para Embarcaciones", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fi000w_4.Boat%20Fenders.pdf?alt=media&token=3cb1eb7a-e4d6-484e-b52e-e2a1c4af7dce" },
+          new BrochureDto { Name = "IRM Company Profile", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fpiokh_1.IRM%20Company%20Profile.pdf?alt=media&token=63045cec-8957-4b7c-9d18-7e4269709603" },
+          new BrochureDto { Name = "IRM Productos Offshore", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fja6ju_5.Offshore%20Products.pdf?alt=media&token=79031ffe-7843-4cc5-8fd9-62dfd025894b" },
+          new BrochureDto { Name = "IRM Energía Eólica", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2F4954f_9.Offshore%20Wind%20Energy.pdf?alt=media&token=01cd131d-754b-416c-ab69-9b58ba9ff72e" },
+          new BrochureDto { Name = "IRM Bitas", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fo5tn2_7.Bollards.pdf?alt=media&token=b769f21a-4864-49f4-bc74-3e56edcef62d" },
+          new BrochureDto { Name = "IRM Accesorios para Muelles", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fqkvdq_6.Dock%20accessories.pdf?alt=media&token=d9e30d8d-1bc3-4543-8471-1ca06eec7963" },
+          new BrochureDto { Name = "IRM Defensas Marinas", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2F21a4r_2.Marine%20Fenders.pdf?alt=media&token=72b6f0e8-e126-4fa6-b0c6-e075c961f8b0" },
+          new BrochureDto { Name = "Man Oil and Marine Catalogue", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Fn9en3_Man%20Oil%20and%20Marine%20Catalogue.pdf?alt=media&token=a84aa7cd-883e-4c97-b3eb-6e3dbad64d1c" },
+          new BrochureDto { Name = "Man Oil Ancillary Equipment", Url = "https://firebasestorage.googleapis.com/v0/b/fir-cms-t1.appspot.com/o/brochures%2Ftarhn_Man%20Oil%20and%20Marine%20Ancillary%20Equipment%20catalogue.pdf?alt=media&token=2fed3e79-0919-4eee-9a21-810587eba7c8" },
+        };
+
+    };
+}
